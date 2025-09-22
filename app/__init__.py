78e0510f10d7ea539019441007cfe89bfa76fa2d
skCNA <skaikai131@gmail.com>
2025-09-22 16:15:06 +0800

    test

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,8 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    # app.config.from_object("config.py")
 
-    # 注册路由
-    from .routes import main_bp
-    app.register_blueprint(main_bp)
+    from .routes import main
+    app.register_blueprint(main)
 
     return app
